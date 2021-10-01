e5c3f0c02e96c21685da3d4a07da8e7d1ba2e5d7
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-01 00:41:54 -0300

    Inicia criaçao da funçao createElement que desconstroi o objeto retornado da pesquisa

--- script/script.js
+++ script/script.js
@@ -1,34 +1,37 @@
 const API_ROOT = 'https://api.smk.dk/api/v1';
 const ENDPOINT_ART_SEARCH = '/art/search/';
 
-async function getArtSearch(key) {
+function createElement({
+  artist, // array | artist[0]
+  colors, // array
+  image_thumbnail, // string
+  titles, // titles['0']['title']
+  production, // array | production[0] --> info about the artist (sex, name, birth, death)
+  production_date, // array | production_date[0].period --> date of production
+  techniques, // array
+}) {
+  console.log(techniques);
+}
+
+async function searchArt(key) {
   const response = await fetch(
     `${API_ROOT}${ENDPOINT_ART_SEARCH}/?keys=${key}&offset=0&rows=2000&lang=en`
   );
   const { items } = await response.json();
   const imagesOnly = items.filter((obj) => obj.has_image === true);
 
-  console.log(imagesOnly);
+  return createElement(imagesOnly[1]);
+  // return console.log(imagesOnly);
 }
 
-function searchArt() {
+function handleSearch() {
   const searchBtn = document.querySelector('#search-btn');
   searchBtn.addEventListener('click', () => {
     const val = document.querySelector('#art-search').value;
-    getArtSearch(val);
+    searchArt(val);
   });
 }
 
-function createElement({
-  artist,
-  colors,
-  image_thumbnail,
-  titles,
-  production,
-  production_date,
-  techiniques,
-}) {}
-
 window.onload = () => {
-  searchArt();
+  handleSearch();
 };
