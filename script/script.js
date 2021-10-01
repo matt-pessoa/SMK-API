f1db0e823c6d54ead22afde8b7bdfa15e62b1d27
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-01 17:24:45 -0300

    Adiciona o titulo da obra ao selecionar

--- script/script.js
+++ script/script.js
@@ -16,10 +16,63 @@ function getSelectedArtwork() {
 function addArtwork() {
   const artworkField = document.querySelector('#artwork-field');
   const artwork = getSelectedArtwork().cloneNode(true);
-  artwork.setAttribute('id', 'art-focus');
+  artwork.classList.add('art-focus');
   artwork.classList.remove('item');
+  artwork.firstElementChild.classList.remove('art-thumb');
 
   artworkField.appendChild(artwork);
+}
+
+//   <section>
+//   <h3>Title</h3>
+//   <p>titulo da obra (1800 - 1900)</p>
+//   <h3>Artist</h3>
+//   <p>nome do artista</p>
+//   <h3>Techiniques</h3>
+//   <p>pintura a oleo</p>
+//   <h3>Color</h3>
+//   <p>cores</p>
+// </section>
+
+function createCard({
+  id,
+  artist, // array | artist[0]
+  colors, // array
+  image_thumbnail, // string
+  titles, // titles['0']['title']
+  production, // array | production[0] --> info about the artist (sex, name, birth, death)
+  production_date, // array | production_date[0].period --> date of production
+  techniques,
+}) {
+  const card = document.createElement('section');
+  card.setAttribute('id', 'artwork-info');
+
+  // Title
+  const artworkTitle = document.createElement('h3');
+  artworkTitle.textContent = `${titles[0]['title']} (${production_date[0]['period']})`;
+  card.appendChild(artworkTitle);
+
+  //
+
+  return card;
+}
+
+async function addArtInfo() {
+  const artwork = document.querySelector('.art-focus');
+  const artworkField = document.querySelector('#artwork-field');
+
+  const search = document.querySelector('#art-search').value;
+
+  const response = await fetch(
+    `${API_ROOT}${ENDPOINT_ART_SEARCH}/?keys=${search}&offset=0&rows=2000&lang=en`
+  );
+  const { items } = await response.json();
+
+  const artworkById = items.filter(
+    (obj) => obj.id === artwork.getAttribute('id')
+  )[0];
+
+  artworkField.appendChild(createCard(artworkById));
 }
 
 function removeClass(field, className) {
@@ -29,6 +82,7 @@ function removeClass(field, className) {
 
 function switchArtFocus() {
   const artworkField = document.querySelector('#artwork-field');
+  artworkField.innerHTML = '';
   if (artworkField.children.length !== 0) {
     artworkField.removeChild(document.querySelector('.artwork-section'));
   }
@@ -43,6 +97,7 @@ function handleArtClick(event) {
   selected.closest('.artwork-section').classList.add('selected');
 
   addArtwork();
+  addArtInfo();
 }
 
 function createElement({
@@ -79,7 +134,7 @@ async function searchArt(key) {
 
   imagesOnly.forEach((obj) => createElement(obj));
 
-  // return imagesOnly;
+  return imagesOnly;
 }
 
 function handleSearchClick() {
