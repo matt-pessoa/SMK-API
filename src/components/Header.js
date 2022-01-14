a70c873ae54de4a760ae98892918b74443ef10a5
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-01-14 20:00:23 -0300

    create selector to switch languages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,37 @@
 import React from "react";
+import i18next from "i18next";
 import { useTranslation } from "react-i18next";
 
 function Header() {
 	const { t } = useTranslation();
 
+	const languages = [
+		{
+			code: "en",
+			name: "English",
+		},
+		{
+			code: "fr",
+			name: "Français",
+		},
+		{
+			code: "pt",
+			name: "Português",
+		},
+	];
+
 	return (
 		<header>
 			<nav>
 				<li>{t("search_header")}</li>
+				<li>{t("themes_header")}</li>
+				<li>{t("my_list_header")}</li>
+				<li>{t("about_project_header")}</li>
+				<select>
+					{languages.map(({ code, name }) => (
+						<option onClick={() => i18next.changeLanguage(code)}>{name}</option>
+					))}
+				</select>
 			</nav>
 		</header>
 	);
