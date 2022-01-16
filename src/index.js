1894268ad89c4ab8de9c9f9c78bcdc833170637e
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-01-16 00:44:55 -0300

    create pages from navbar

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import "./i18n";
 
 import Home from "./pages/Home";
 import Themes from "./pages/Themes";
+import Favorites from "./pages/Favorites";
+import About from "./pages/About";
 
 ReactDOM.render(
 	<BrowserRouter>
 		<Routes>
 			<Route exact path="/" element={<Home />} />
 			<Route path="/themes" element={<Themes />} />
+			<Route path="/favorites" element={<Favorites />} />
+			<Route path="/about" element={<About />} />
 		</Routes>
 	</BrowserRouter>,
 	document.getElementById("root")
